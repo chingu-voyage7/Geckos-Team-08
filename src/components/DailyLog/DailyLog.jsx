5b69495f81b97e3b9c66361b04a08dd85dfa1be9
gillenha <gillen08@gmail.com>
2018-12-03 00:29:51 -0500

    built daily log

--- src/components/DailyLog/DailyLog.jsx
+++ src/components/DailyLog/DailyLog.jsx
@@ -1,10 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import moment from "moment";
+import List from "./List";
+import "./DailyLog.css";
 
 class DailyLog extends Component {
-  state = {  }
-  render() { 
-    return (  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{moment().format("MMMM Do")} </h1>
+        <p>enter a task for today and hit submit</p>
+        <List items={this.state.items} />
+        <form onSubmit={this.onSubmit}>
+          {/* <input value={this.state.term} onChange={this.onChange} /> */}
+          <button>submit</button>
+        </form>
+      </div>
+    );
   }
 }
- 
+
 export default DailyLog;
